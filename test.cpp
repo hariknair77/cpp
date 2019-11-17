513b8a2bfd8f2907c58f19d6a9a424fff39079e5
hariknair77 <hariknair77@gmail.com>
2019-11-17 16:48:32 +0530

    to bin

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bitset>
+#include <string>
+using namespace std;
+int main(){
+    // int a[] ={1,2,3,4,5};
+    // for(auto i:a)
+    //     i++;
+    // cout<<a[0]<<endl;
+    // for(auto &i:a)
+    //     i++;
+    // cout<<a[0]<<endl;
+    // string bin = bitset<4>(4).to_string();
+    // cout<<bin<<endl;
+    string name ="00000001";
+    // cout<<name.find_first_not_of('0');
+    if(cout<<name[100])
+        cout<<"oopss";
+    return 0;
+
+}
